75dfc19b32bd0bb57a54cc7b95c28315f04f7820
Gary B <grider27@gmail.com>
2021-05-13 22:35:54 -0400

    working footer and styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Home from "./components/Home";
 //import About from './components/About';
 //import Projects from './components/Projects';
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 //import Contact from './components/Contact';
 import './App.css';
 
@@ -19,7 +19,7 @@ function App() {
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} /> */}
         </Switch>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </Router>
   );
